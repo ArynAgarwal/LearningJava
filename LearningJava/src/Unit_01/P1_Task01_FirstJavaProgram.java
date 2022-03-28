3305cf210ba1faceea88aab28a3dd95c0962218d
HP <HP@LAPTOP-Q9OPEMMS>
2022-03-28 23:54:05 +0530

    First Commmit!

--- LearningJava/src/Unit_01/P1_Task01_FirstJavaProgram.java
+++ LearningJava/src/Unit_01/P1_Task01_FirstJavaProgram.java
@@ -3,7 +3,8 @@ package Unit_01;
 public class P1_Task01_FirstJavaProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
 
 	}
 
--- LearningJava/src/Unit_01/P1_Task02_FirstJavaProgramInTerminal.java
+++ LearningJava/src/Unit_01/P1_Task02_FirstJavaProgramInTerminal.java
@@ -3,8 +3,8 @@ package Unit_01;
 public class P1_Task02_FirstJavaProgramInTerminal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Hello World. I am a terminal.");
 	}
 
 }
