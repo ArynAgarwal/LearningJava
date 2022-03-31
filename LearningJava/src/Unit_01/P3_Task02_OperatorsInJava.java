5b44e238d5b28783891dee5295484450a2dca5a5
HP <HP@LAPTOP-Q9OPEMMS.mshome.net>
2022-04-01 01:08:33 +0530

    P3 Task02 Commit!

---
+++ LearningJava/src/Unit_01/P3_Task02_OperatorsInJava.java
@@ -0,0 +1,23 @@
+package Unit_01;
+
+public class P3_Task02_OperatorsInJava {
+
+	public static void main(String[] args) {
+		
+		P3_Task02_OperatorsInJava obj = new P3_Task02_OperatorsInJava();
+		obj.UnaryOperator();
+	}
+	
+	void UnaryOperator() {
+		int a = 10;
+		boolean b = false;
+		System.out.println("Inside Unary Operator");
+		System.out.println(a++);
+		System.out.println(a--);
+		System.out.println(++a);
+		System.out.println(--a);
+		System.out.println("----------------------");
+	}
+	
+//	void Arithematic
+}
