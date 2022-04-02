70c5c336bd17dc71e14c10c7f54ae3f19fd865c1
HP <HP@LAPTOP-Q9OPEMMS.mshome.net>
2022-04-02 22:00:27 +0530

    P3 Task02 Commit!

--- LearningJava/src/Unit_01/P3_Task02_OperatorsInJava.java
+++ LearningJava/src/Unit_01/P3_Task02_OperatorsInJava.java
@@ -57,5 +57,30 @@ public class P3_Task02_OperatorsInJava {
 		System.out.println("-----------------------");
 	}
 	
-	
+	void RelationalOperators()
+	 {
+		System.out.println("Relational Operators - ");
+		int a=3,b=3,c=5;
+		System.out.println("a==b = " + (a==b));
+		System.out.println("a!=b = " + (a!=b));
+		System.out.println("a>c = " + (a>c));
+		System.out.println("a<=c = " + (a<=c));
+		System.out.println("-----------------------");
+	}
+	 
+	 
+	 void BitWiseOperatorsAndLogical()
+	 {
+		int a=5,b=10,c=20;
+		System.out.println("Bitwise and Logical Operators - ");
+		System.out.println(a<b && b++<c);
+		System.out.println(a++>b || b>c);
+		System.out.println(b);
+		System.out.println(a);
+		System.out.println(a>b | a<c);
+		System.out.println(a<b & a++<c);
+		System.out.println(a);
+		System.out.println("-----------------------");
+	 }
+
 }
