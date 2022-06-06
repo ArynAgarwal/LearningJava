753b7aaf7aee1109b5b941d7a4bd65b2fee3f9a4
HP <HP@LAPTOP-Q9OPEMMS>
2022-06-07 00:11:33 +0530

    Unit 05 Commit!!

--- LearningJava/src/Unit_05/SampleClass.java
+++ LearningJava/src/Unit_05/SampleClass.java
@@ -8,18 +8,13 @@ public class SampleClass {
 		Statement myStmt = null;
 		ResultSet myRs = null;
 		try {
-			myConn =
-					DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcDatabase
-							", "root" , "root@123");
-							myStmt = myConn.createStatement();
-							int a = myStmt.executeUpdate("INSERT INTO jdbc_tbl VALUES (5, 'Mysql',
-									'Akbar', '2021-02-18')");
-									myRs = myStmt.executeQuery("select * from jdbc_tbl");
-							while (myRs.next()) {
-								System.out.println(myRs.getString("jdbc_id") + ", " +
-										myRs.getString("title") + ", " + myRs.getString("author") + ", " +
-										myRs.getString("submission_date"));
-							}
+			myConn =DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcDatabase", "root" , "root@123");
+			myStmt = myConn.createStatement();
+			int a = myStmt.executeUpdate("INSERT INTO jdbc_tbl VALUES (5, 'Mysql','Akbar', '2021-02-18')");
+			myRs = myStmt.executeQuery("select * from jdbc_tbl");
+			while (myRs.next()) {
+					System.out.println(myRs.getString("jdbc_id") + ", " +myRs.getString("title") + ", " + myRs.getString("author") + ", " +myRs.getString("submission_date"));
+			}
 		}
 		catch (Exception exc) {
 			exc.printStackTrace();
