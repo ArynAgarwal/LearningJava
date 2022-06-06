13ab475d19f9d0a82b857f4485aa63c4102ad9f1
HP <HP@LAPTOP-Q9OPEMMS>
2022-06-07 00:09:09 +0530

    Unit 05 Commit!!

--- LearningJava/src/Unit_05/SampleClass.java
+++ LearningJava/src/Unit_05/SampleClass.java
@@ -1,43 +1,33 @@
 package Unit_05;
-//
-//public class SampleClass {
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//}
-//
 
 //package com.java.jdbc;
 import java.sql.*;
 public class SampleClass {
-public static void main(String[] args) throws SQLException {
-Connection myConn = null;
-Statement myStmt = null;
-ResultSet myRs = null;
-try {
-myConn =
-DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcDatabase
-", "root" , "root@123");
-myStmt = myConn.createStatement();
-int a = myStmt.executeUpdate("INSERT INTO jdbc_tbl VALUES (5, 'Mysql',
-'Akbar', '2021-02-18')");
-myRs = myStmt.executeQuery("select * from jdbc_tbl");
-while (myRs.next()) {
-System.out.println(myRs.getString("jdbc_id") + ", " +
-myRs.getString("title") + ", " + myRs.getString("author") + ", " +
-myRs.getString("submission_date"));
-}
-}
-catch (Exception exc) {
-exc.printStackTrace();
-}
-finally {
-myRs.close();
-myStmt.close();
-myConn.close();
-}
-}
+	public static void main(String[] args) throws SQLException {
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		try {
+			myConn =
+					DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcDatabase
+							", "root" , "root@123");
+							myStmt = myConn.createStatement();
+							int a = myStmt.executeUpdate("INSERT INTO jdbc_tbl VALUES (5, 'Mysql',
+									'Akbar', '2021-02-18')");
+									myRs = myStmt.executeQuery("select * from jdbc_tbl");
+							while (myRs.next()) {
+								System.out.println(myRs.getString("jdbc_id") + ", " +
+										myRs.getString("title") + ", " + myRs.getString("author") + ", " +
+										myRs.getString("submission_date"));
+							}
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		finally {
+			myRs.close();
+			myStmt.close();
+			myConn.close();
+		}
+	}
 }
