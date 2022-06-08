600f13caa391dc7968b6b81a3ffb335df5be93be
HP <HP@LAPTOP-Q9OPEMMS>
2022-06-09 01:00:32 +0530

    Unit_05 Commit!!

--- LearningJava/src/Unit_05/SampleClass.java
+++ LearningJava/src/Unit_05/SampleClass.java
@@ -11,6 +11,7 @@ public class SampleClass {
 			myConn =DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcDatabase", "root" , "");
 			myStmt = myConn.createStatement();
 			int a = myStmt.executeUpdate("INSERT INTO jdbc_tbl VALUES (5, 'Mysql','Akbar', '2021-02-18')");
+//			int a = myStmt.executeUpdate("DELETE FROM jdbc_tbl WHERE jdbc_id='5'");
 			myRs = myStmt.executeQuery("select * from jdbc_tbl");
 			while (myRs.next()) {
 					System.out.println(myRs.getString("jdbc_id") + ", " +myRs.getString("title") + ", " + myRs.getString("author") + ", " +myRs.getString("submission_date"));
